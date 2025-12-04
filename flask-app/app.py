f23da9834ccaf137ab26acf5c5dc0c56ca00e932
meesagopi25 <meesagopi25@gmail.com>
2025-12-04 16:59:17 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,1 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Flask CI/CD on OpenShift via Jenkins!", 200
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
